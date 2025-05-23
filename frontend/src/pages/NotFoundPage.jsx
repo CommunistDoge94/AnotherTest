c9bdcec2a0658829fc039ea4c900b0886ac28a55
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-05-23 18:10:12 +0300

    II. Создание страницы 404

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <div className="container mt-5">
+    <h1>404</h1>
+    <p>Страница не найдена</p>
+  </div>
+);
+
+export default NotFoundPage;
